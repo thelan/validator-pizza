31a020b5a2e7661e05994dd02db6222c510a7608
Julien Recurt <thelan@servme.fr>
2017-09-08 09:04:35 +0200

    Fix case typo

--- DependencyInjection/ValidatorPizzaExtension.php
+++ DependencyInjection/ValidatorPizzaExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
-class validatorPizzaExtension extends Extension
+class ValidatorPizzaExtension extends Extension
 {
     /**
      * {@inheritdoc}
